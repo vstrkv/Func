9fdbc93a53c89acd0061ba6266434158854a68c8
vstrkv <37896140+vstrkv@users.noreply.github.com>
2021-04-29 01:17:16 +0700

    Add files via upload

---
+++ job_task/src/Task1.java
@@ -0,0 +1,47 @@
+public class Task1 {
+    public static void main(String[] args) {
+
+        MyFunc<?> myFunc = new MyFunc() {
+            @Override
+            public int testFunc(int param1, int param2) {
+                return param1 - param2;
+            }
+        };
+
+        MyFunc<?> myFunc2 = ((param1, param2) -> param1 + param2);
+
+
+        MyFunc<Boolean> myFunc3 = MyFunc::kek;
+
+
+        System.out.print("5 - 3 = ");
+        testFunc(myFunc, 5, 3);
+
+        System.out.print("5 + 3 = ");
+        testFunc(myFunc2, 5,3);
+
+        System.out.print("5 * 3 = ");
+        testFunc(myFunc3, 5, 3 );
+
+    }
+
+    public void myFunc3(MyFunc<?> myFunc3) {
+        myFunc3.testFunc(5,3);
+    }
+
+
+
+    public static <T> void testFunc(MyFunc<T> f, int v1, int v2) {
+        System.out.println(f.testFunc(v1, v2));
+    }
+
+
+}
+@FunctionalInterface
+interface MyFunc<T> {
+    public int testFunc(int param1, int param2);
+
+    static int kek(int i, int i1) {
+        return i * i1;
+    }
+}
